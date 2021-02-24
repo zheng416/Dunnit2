15e3cfead147cecdfc4d00425bc03b3e8a8c43be
Jacky Zheng <zheng416@purdue.edu>
2021-02-24 14:58:32 -0500

    Implemented Persistent Storage for Local Store; Implemented different sections, checking and deleting

--- Dunnit2/ViewControllers/HomeViewController.swift
+++ Dunnit2/ViewControllers/HomeViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import CoreData
 
 class HomeViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
-    var tasks = [myTask]()
+    var taskStore = [[TaskEntity](), [TaskEntity]()]
+    
+    func getData() {
+        let tasks = DataBaseHelper.shareInstance.fetch()
+        
+        taskStore = [tasks.filter{$0.isDone == false}, tasks.filter{$0.isDone == true}]
+        
+        tableView.reloadData()
+    }
     
     @IBAction func didTapAdd() {
         // Show add vc
@@ -19,15 +28,14 @@ class HomeViewController: UIViewController {
         guard let vc = storyboard.instantiateViewController(identifier: "add") as? AddViewController else {
             return
         }
-        
+
         vc.title = "New Task"
         vc.navigationItem.largeTitleDisplayMode = .never
         vc.completion = {title, body, date in
             DispatchQueue.main.async {
                 self.navigationController?.popToRootViewController(animated: true)
-                let new = myTask(title: title, date: date, identifier: "id_\(title)")
-                self.tasks.append(new)
-                self.tableView.reloadData()
+                DataBaseHelper.shareInstance.save(title: title, body: body, date: date, isDone: false)
+                self.getData()
             }
         }
         navigationController?.pushViewController(vc, animated: true)
@@ -39,8 +47,9 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light
         // Do any additional setup after loading the view.
-        tableView.delegate = self
-        tableView.dataSource = self
+        getData()
+//        tableView.delegate = self
+//        tableView.dataSource = self
     }
     
 
@@ -66,30 +75,63 @@ extension HomeViewController: UITableViewDelegate {
 }
 
 extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "To-do" : "Done"
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return taskStore.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tasks.count
+        return taskStore[section].count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let date = tasks[indexPath.row].date
+        let date = taskStore[indexPath.section][indexPath.row].date!
         let formatter = DateFormatter()
         formatter.dateFormat = "MMM dd, YYYY"
-        cell.textLabel?.text = tasks[indexPath.row].title
+        cell.textLabel?.text = taskStore[indexPath.section][indexPath.row].title
         cell.detailTextLabel?.text = formatter.string(from: date)
         
         return cell
     }
 }
 
-struct myTask {
-    let title: String
-    let date: Date
-    let identifier: String
+extension HomeViewController {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let doneAction = UIContextualAction(style: .normal, title: nil) { (action, sourceView, completionHandler) in
+            let row = self.taskStore[0][indexPath.row]
+            
+            DataBaseHelper.shareInstance.update(title: row.title!, isDone: true)
+            
+            self.getData()
+        }
+        // doneAction.image = UIImage(named: "")
+        doneAction.backgroundColor = .cyan
+        return indexPath.section == 0 ? UISwipeActionsConfiguration(actions: [doneAction]) : nil
+    }
     
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .normal, title: nil) { (action, sourceView, completionHandler) in
+            let row = self.taskStore[indexPath.section][indexPath.row]
+            
+            DataBaseHelper.shareInstance.deleteData(title: row.title!)
+            
+            self.getData()
+        }
+        // deleteAction.image = UIImage(named: "")
+        deleteAction.backgroundColor = .red
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
 }
+
+//struct myTask {
+//    var title: String
+//    var date: Date
+//    var identifier: String
+//    var isDone: Int
+//}
