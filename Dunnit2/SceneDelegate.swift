1220ee5e5afc02dbe18bd91e9535828595ce93ce
Jacky Zheng <zheng416@purdue.edu>
2021-03-01 14:57:06 -0500

    Changed Setting Scene

--- Dunnit2/SceneDelegate.swift
+++ Dunnit2/SceneDelegate.swift
@@ -28,7 +28,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, GIDSignInDelegate {
             if Auth.auth().currentUser != nil {
                 print("HEREEEEEEEEEEEEEEEEEEEEEEEEEE", Auth.auth().currentUser)
                 // redirect to home controller
-                self.window!.rootViewController = homeStory.instantiateViewController(withIdentifier: "main")
+//                self.window!.rootViewController = homeStory.instantiateViewController(withIdentifier: "main")
+                let storyboard = UIStoryboard(name: "Settings", bundle: nil)
+                self.window!.rootViewController = storyboard.instantiateViewController(withIdentifier: "settings")
                 self.window?.makeKeyAndVisible()
             } else {
                 // redirect to login controller
