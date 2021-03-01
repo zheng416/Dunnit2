ed83121f1488b78326b813000531c52c94480f43
Jacky Zheng <zheng416@purdue.edu>
2021-03-01 13:08:01 -0500

    Sidebar

---
+++ Dunnit2/ViewControllers/MenuViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MenuViewController.swift
+//  Dunnit2
+//
+//  Created by Jacky Zheng on 3/1/21.
+//
+
+import UIKit
+
+enum MenuType: Int {
+    case all
+    case shared
+    case myList
+    case settings
+    
+}
+
+class MenuViewController: UITableViewController {
+    
+    var didTapMenuType: ((MenuType) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let menuType = MenuType(rawValue: indexPath.row) else { return }
+        dismiss(animated: true) { [weak self] in
+            print("Dismissing: \(menuType)")
+            self?.didTapMenuType?(menuType)
+        }
+    }
+
+}
