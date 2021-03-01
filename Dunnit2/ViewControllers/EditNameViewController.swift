9bdf7417d1f27e8935f6f5ab5354a7bb3dac27b0
Andrew T. Lim <lim243@purdue.edu>
2021-02-28 21:54:19 -0500

    Set core data with name update

---
+++ Dunnit2/ViewControllers/EditNameViewController.swift
@@ -0,0 +1,80 @@
+//
+//  EditNameViewController.swift
+//  Dunnit2
+//
+//  Created by Andrew T Lim on 2/28/21.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class EditNameViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var doneEditButton: UIButton!
+    
+    var userStore = [UserEntity]()
+    
+    // Custom check function
+    public var completion: ((String) -> Void)?
+    
+    //  Access databse functions
+    func getUser() -> [String: Any] {
+        let user = DataBaseHelper.shareInstance.fetchUser()
+        
+        // Unpack user entity to dictionary
+        var endUser = [String:Any]()
+        for x in user as [UserEntity] {
+            endUser["name"] = x.name
+            endUser["email"] = x.email
+            endUser["darkMode"] = x.darkMode
+            endUser["notifications"] = x.notifications
+            endUser["sound"] = x.sound
+        }
+        
+        return endUser
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let user = getUser()
+        
+        nameField.text = user["name"] as! String
+        nameField.returnKeyType = .done
+        nameField.autocapitalizationType = .words
+        nameField.autocorrectionType = .no
+        nameField.becomeFirstResponder()
+        nameField.delegate = self
+    }
+    
+    @IBAction func buttonTapped() {
+        nameField.resignFirstResponder()
+        // Save stuff here?
+        if let name = nameField.text {
+            print("Text is \(name)")
+            // Update name here
+            
+            DispatchQueue.main.async {
+                // TODO: Add routing after hit done
+                
+                // Create a new user should be used in create account
+    
+                let user = self.getUser()
+                // Update Name based on email
+                DataBaseHelper.shareInstance.updateName(name: name, email: user["email"] as! String)
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameField.resignFirstResponder()
+        // Save stuff here?
+        if let text = nameField.text {
+            print("Text is \(text)")
+            // Update name here
+        }
+        return true
+    }
+}
--- Dunnit2/ViewControllers/SettingsViewController.swift
+++ Dunnit2/ViewControllers/SettingsViewController.swift
@@ -8,14 +8,35 @@ import Foundation
 import UIKit
 
 class SettingsViewController: UITableViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    var userStore = [UserEntity]()
+    
+    //  Access databse functions
+    func getUser() -> [String: Any] {
+        let user = DataBaseHelper.shareInstance.fetchUser()
+        
+        // Unpack user entity to dictionary
+        var endUser = [String:Any]()
+        for x in user as [UserEntity] {
+            endUser["name"] = x.name
+            endUser["email"] = x.email
+            endUser["darkMode"] = x.darkMode
+            endUser["notifications"] = x.notifications
+            endUser["sound"] = x.sound
+        }
+        
+        return endUser
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let user = getUser()
+        
+        nameLabel.text =  user["name"] as! String
+        emailLabel.text = user["email"] as! String
     }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("The selected section is \(indexPath.section)")
-    }
-    
-
-    
 }
